8f031b873da01fb584ba1fcfdd0202839f09ad12
Sandesh Rimal <sandesh.rimal@bison.howard.edu>
2019-10-31 18:55:55 -0400

    Starting main here

--- project1.s
+++ project1.s
@@ -2,3 +2,10 @@
 	input: 	    	.space		11       # Input contains 10 chars plus an endline char.
     inputPrompt:    .asciiz     "Input 10 charactered alphanumeric string: "
     newLine: 		.asciiz 	"\n"
+.text # Text section begins here. 
+  	main:
+	# Prompt the user to input a 10 char aplhanumeric string.
+	    li $v0, 4
+	    la $a0, inputPrompt
+	    syscall
+
