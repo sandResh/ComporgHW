94359973aeae114411783abdc23e5bf4ef513988
Sandesh Rimal <sandesh.rimal@bison.howard.edu>
2019-10-31 18:45:35 -0400

    First commit. Initializing input , inputprompt and new line

---
+++ project1.s
@@ -0,0 +1,4 @@
+.data # Data section begins here.
+	input: 	    	.space		11       # Input contains 10 chars plus an endline char.
+    inputPrompt:    .asciiz     "Input 10 charactered alphanumeric string: "
+    newLine: 		.asciiz 	"\n"
